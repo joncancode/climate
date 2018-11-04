6999722344527f6cb4ef463afba9d8f2ba593112
joncancode <jc.haines19@gmail.com>
2018-11-03 22:42:18 -0400

    add netlify task

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 const gulp = require('gulp'),
   browserSync = require('browser-sync').create(),
   sass = require('gulp-sass');
-  tinypng = require('gulp-tinypng-compress');
+  tinypng = require('gulp-tinypng-compress'),
+  netlify = require('gulp-netlify')
 
 // Sass Task
 gulp.task('sass', function() {
@@ -23,6 +24,15 @@ gulp.task('sass', function() {
         .pipe(gulp.dest('build/img'))
         .pipe(browserSync.stream());
 });
+
+// Netlify
+gulp.task('deploy', function () {
+  gulp.src('./public/**/*')
+    .pipe(netlify({
+      site_id: '1461a221-a1d3-4588-9545-5c8fc9ca3320',
+      access_token: '2ce68af73b145a6aa86ccc51296b680edc8a053ddac3e9ee14c1edfce08d27d3'
+    }))
+})
 
 // Serve Task
 gulp.task('serve', ['sass', 'image'], function() {
